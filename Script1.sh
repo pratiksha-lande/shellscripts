cbeb4527fd2e53e4f63e6ba4820312b8be5393c3
Pratiksha <pratilande123@gmail.com>
2024-06-25 15:41:02 +0530

    this is my first commit feature branch to main branch

--- Script1.sh
+++ Script1.sh
@@ -4,4 +4,6 @@ echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "this is my first commit"
+echo "this is modifind to script1"
+echo "this code push to main branch"
 
